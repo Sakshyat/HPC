ee837298981245ed4683839025d1ee2429cd21d5
Sakshyat <58883624+Sakshyat@users.noreply.github.com>
2020-12-23 19:01:18 +0545

    Add files via upload

--- 6cs005_PortfolioS1_20_21_2036886_Sakshyat_Sharma/2036886_Task2_C_1.c
+++ 6cs005_PortfolioS1_20_21_2036886_Sakshyat_Sharma/2036886_Task2_C_1.c
@@ -5,66 +5,61 @@
 #include <unistd.h>
 #include <time.h>
 
-
 /******************************************************************************
-  Demonstrates how to crack an encrypted password using a simple
-  "brute force" algorithm. Works on passwords that consist only of 2 uppercase
-  letters and a 2 digit integer.
+
+  Sakshyat Sharma, 2036886
+  
+  Task2_C_1:
+  Crack an encrypted password that consist only of 2 uppercase letters and a 2 digit integer.
 
   Compile with:
     cc -o 2036886_Task2_C_1 2036886_Task2_C_1.c -lcrypt
-
-  If you want to analyse the output then use the redirection operator to send
-  output to a file that you can view using an editor or the less utility:
+  
+  Run with:
     ./2036886_Task2_C_1
 
-  Dr Kevan Buckley, University of Wolverhampton, 2018 Modified by Dr. Ali Safaa 2019
 ******************************************************************************/
 
 int count=0;     // A counter used to track the number of combinations explored so far
+char *enc_password = "$6$AS$v3aoGpYBSLWN3oLWkWXyVcjOgbCDTjqig1h0Rl8TOsYs.Jtbksd/tw4jMNmpaUouWg9LJRQdJtu4ZJBEQAwA11";  //encrypted password
 
-/**
- Required by lack of standard function in C.   
-*/
+void substr(char *dest, char *src, int start, int length)
+{
 
-void substr(char *dest, char *src, int start, int length){
   memcpy(dest, src + start, length);
   *(dest + length) = '\0';
+  
 }
 
-/**
- This function can crack the kind of password explained above. All combinations
- that are tried are displayed and when the password is found, #, is put at the 
- start of the line. Note that one of the most time consuming operations that 
- it performs is the output of intermediate results, so performance experiments 
- for this kind of program should not include this. i.e. comment out the printfs.
-*/
 
-void crack(char *salt_and_encrypted){
-  int x, y, z;     // Loop counters
-  char salt[7];    // String used in hashing the password. Need space for \0 // incase you have modified the salt value, then should modifiy the number accordingly
-  char plain[7];   // The combination of letters currently being checked // Please modifiy the number when you enlarge the encrypted password.
+void crack_pass(char *salt_and_encrypted)
+{
+
+  int s, a, k;     // Loop counters
+  char salt[7];    // String used in hashing the password. Need space for \0
+  char plain[7];   // The combination of letters currently being checked
   char *enc;       // Pointer to the encrypted password
 
   substr(salt, salt_and_encrypted, 0, 6);
 
-   for(x='A'; x<='Z'; x++){
-    for(y='A'; y<='Z'; y++){
-      for(z=0; z<=99; z++){
-        sprintf(plain, "%c%c%02d", x, y, z); 
+   for(s='A'; s<='Z'; s++){
+    for(a='A'; a<='Z'; a++){
+      for(k=0; k<=99; k++){
+        sprintf(plain, "%c%c%02d", s, a, k); 
         enc = (char *) crypt(plain, salt);
         count++;
         if(strcmp(salt_and_encrypted, enc) == 0){
 	    printf("#%-8d%s %s\n", count, plain, enc);
-	    return;
         } 
       }
     }
   }
+  
 }
 
-int time_difference(struct timespec *start, struct timespec *finish, 
-                              long long int *difference) {
+int time_difference(struct timespec *start, struct timespec *finish,long long int *difference) 
+{
+
   long long int ds =  finish->tv_sec - start->tv_sec; 
   long long int dn =  finish->tv_nsec - start->tv_nsec; 
 
@@ -72,26 +67,30 @@ int time_difference(struct timespec *start, struct timespec *finish,
     ds--;
     dn += 1000000000; 
   } 
+  
   *difference = ds * 1000000000 + dn;
+  
   return !(*difference > 0);
+  
 }
 
 
-int main(int argc,char *argv[]){
-  int i;
+int main(int argc,char *argv[])
+{
 
-struct timespec start, finish;
-long long int time_elapsed;
+  struct timespec start, finish;
+  long long int time_elapsed;
 
   clock_gettime(CLOCK_MONOTONIC, &start);
 
-  crack("$6$AS$3tO1nv6UTBEiADlwQasACiyiFj4dZIhd/hkyeBXiYaBMgrHx4HzSiDMhDzqM/C1dtGFfvqNSCEasynunym83T/");
+  crack_pass(enc_password);
 
   clock_gettime(CLOCK_MONOTONIC, &finish);
   time_difference(&start, &finish, &time_elapsed);
-  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed, 
-         (time_elapsed/1.0e9)); 
+  
+  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed, (time_elapsed/1.0e9)); 
 
   return 0;
+  
 }
 
--- 6cs005_PortfolioS1_20_21_2036886_Sakshyat_Sharma/2036886_Task2_C_5.c
+++ 6cs005_PortfolioS1_20_21_2036886_Sakshyat_Sharma/2036886_Task2_C_5.c
@@ -8,109 +8,110 @@
 #include <math.h>
 
 /******************************************************************************
-  Demonstrates how to crack an encrypted password using a simple
-  "brute force" algorithm. Works on passwords that consist only of 2 uppercase
-  letters and a 2 digit integer.
+
+   Sakshyat Sharma, 2036886
+   
+   Task2_C_5:
+   Crack an encrypted password that consist only of 2 uppercase letters and a 2 digit integer using multithread.
 
   Compile with:
 	cc -o 2036886_Task2_C_5 2036886_Task2_C_5.c -lcrypt -pthread 
 
-  If you want to analyse the output then use the redirection operator to send
-  output to a file that you can view using an editor or the less utility:
+  Run with:
     	./2036886_Task2_C_5
 
-  Dr Kevan Buckley, University of Wolverhampton, 2018 Modified by Dr. Ali Safaa 2019
 ******************************************************************************/
 
 
-char *encrypted= "$6$AS$3tO1nv6UTBEiADlwQasACiyiFj4dZIhd/hkyeBXiYaBMgrHx4HzSiDMhDzqM/C1dtGFfvqNSCEasynunym83T/";  
+char *enc_password= "$6$AS$v3aoGpYBSLWN3oLWkWXyVcjOgbCDTjqig1h0Rl8TOsYs.Jtbksd/tw4jMNmpaUouWg9LJRQdJtu4ZJBEQAwA11";  
 pthread_mutex_t mutex;
 
 
-void substr(char *dest, char *src, int start, int length){
+void substr(char *dest, char *src, int start, int length)
+{
+
   memcpy(dest, src + start, length);
   *(dest + length) = '\0';
+  
 }
 
-void threads()
+void thread_def()
 {
-pthread_mutex_init(&mutex, NULL);	
-  pthread_t thread_1,thread_2;
+
+  pthread_mutex_init(&mutex, NULL);	
+  pthread_t pthread1,pthread2;
   void *kernel_function_1();
   void *kernel_function_2();
   
-  pthread_create(&thread_1, NULL,(void*)kernel_function_1, encrypted); 
-  pthread_create(&thread_2, NULL,(void*)kernel_function_2, encrypted);
+  pthread_create(&pthread1, NULL,(void*)kernel_function_1, enc_password); 
+  pthread_create(&pthread2, NULL,(void*)kernel_function_2, enc_password);
   
-  pthread_join(thread_1, NULL);
-  pthread_join(thread_2, NULL);
+  pthread_join(pthread1, NULL);
+  pthread_join(pthread2, NULL);
 
 }
 
-/**
- This function can crack the kind of password explained above. All combinations
- that are tried are displayed and when the password is found, #, is put at the 
- start of the line. Note that one of the most time consuming operations that 
- it performs is the output of intermediate results, so performance experiments 
- for this kind of program should not include this. i.e. comment out the printfs.
-*/
+void *kernel_function_1(char *salt_and_encrypted)
+{
 
-
-
-
-
-void *kernel_function_1(char *salt_and_encrypted){
-  int x,y,z;     // Loop counters
-  char salt[7];    // String used in hashing the password. Need space for \0 // incase you have modified the salt value, then should modifiy the number accordingly
-  char plain[7];   // The combination of letters currently being checked // Please modifiy the number when you enlarge the encrypted password.
+  int s,a,k;     // Loop counters
+  char salt[7];    // String used in hashing the password. Need space for \0 
+  char plain[7];   // The combination of letters currently being checked 
   char *enc;  
-   int count = 0;     // Pointer to the encrypted password
+  int count = 0;     // Pointer to the encrypted password
 
   substr(salt, salt_and_encrypted, 0, 6);
+  
   pthread_mutex_lock(&mutex);
-  for(x='A'; x<='M'; x++){
-    for(y='A'; y<='Z'; y++){
-        for(z=0; z<=99; z++){
-          sprintf(plain, "%c%c%02d", x, y, z); 
-          enc = (char *) crypt(plain, salt);
-          count++;
-          if(strcmp(salt_and_encrypted, enc) == 0){
-            printf("#%-8d%s %s\n", count, plain, enc);
-              //return; //uncomment this line if you want to speed-up the running time, program will find you the cracked password only without exploring all possibilites
-          } 
+  
+   for(s='A'; s<='M'; s++){
+    for(a='A'; a<='Z'; a++){
+      for(k=0; k<=99; k++){
+        sprintf(plain, "%c%c%02d", s, a, k); 
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+	    printf("#%-8d%s %s\n", count, plain, enc);
         } 
       }
     }
-    pthread_mutex_unlock(&mutex);
   }
-  void *kernel_function_2(char *salt_and_encrypted){
-  int x,y,z;     // Loop counters
-  char salt[7];    // String used in hashing the password. Need space for \0 // incase you have modified the salt value, then should modifiy the number accordingly
-  char plain[7];   // The combination of letters currently being checked // Please modifiy the number when you enlarge the encrypted password.
+    pthread_mutex_unlock(&mutex);
+    
+  }
+  
+ void *kernel_function_2(char *salt_and_encrypted)
+ {
+ 
+  int s,a,k;     // Loop counters
+  char salt[7];    // String used in hashing the password. Need space for \0 
+  char plain[7];   // The combination of letters currently being checked
   char *enc;  
   int count = 0;       // Pointer to the encrypted password
 
   substr(salt, salt_and_encrypted, 0, 6);
+  
   pthread_mutex_lock(&mutex);
 
-  for(x='N'; x<='Z'; x++){
-    for(y='A'; y<='Z'; y++){
-        for(z=0; z<=99; z++){
-          sprintf(plain, "%c%c%02d", x, y, z); 
-          enc = (char *) crypt(plain, salt);
-          count++;
-          if(strcmp(salt_and_encrypted, enc) == 0){
+   for(s='N'; s<='Z'; s++){
+    for(a='A'; a<='Z'; a++){
+      for(k=0; k<=99; k++){
+        sprintf(plain, "%c%c%02d", s, a, k); 
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
 	    printf("#%-8d%s %s\n", count, plain, enc);
-		        //return;	//uncomment this line if you want to speed-up the running time, program will find you the cracked password only without exploring all possibilites
-          } 
         } 
       }
     }
+  }
     pthread_mutex_unlock(&mutex);
+    
   }
 
-int time_difference(struct timespec *start, struct timespec *finish, 
-                              long long int *difference) {
+int time_difference(struct timespec *start, struct timespec *finish,long long int *difference) 
+{
+
   long long int ds =  finish->tv_sec - start->tv_sec; 
   long long int dn =  finish->tv_nsec - start->tv_nsec; 
 
@@ -118,21 +119,30 @@ int time_difference(struct timespec *start, struct timespec *finish,
     ds--;
     dn += 1000000000; 
   } 
+  
   *difference = ds * 1000000000 + dn;
+  
   return !(*difference > 0);
+  
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+
   struct timespec start, finish;
-  long long int time_elapsed;   
+  long long int time_elapsed;  
+   
   clock_gettime(CLOCK_MONOTONIC, &start);
-  threads();
+  
+  thread_def();
+  
   clock_gettime(CLOCK_MONOTONIC, &finish); 
   time_difference(&start, &finish, &time_elapsed);
-  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed, 
-         (time_elapsed/1.0e9)); 
+  
+  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed,(time_elapsed/1.0e9)); 
 
   return 0;
+  
 }
 
 
